dfff2b92e946a6cc1cafcdca231ca3cdffe2f406
Pavlo <pavlof66@gmail.com>
2018-11-06 15:47:18 +0400

    [-] Убрал registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Provider } from "react-redux";
 import { store } from "./store/configureStore";
 import App from "./components/App";
 
-import registerServiceWorker from "./registerServiceWorker";
-
 import "./index.css";
 
 ReactDOM.render(
@@ -14,4 +12,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root"),
 );
-registerServiceWorker();
